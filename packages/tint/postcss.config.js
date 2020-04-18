cccbef86576058879ed8250be2c0e8ed3d1bb13b
mjeightyfive <mjeightyfive@gmail.com>
2020-04-18 18:03:53 +0100

    tweaks to base css

--- packages/tint/postcss.config.js
+++ packages/tint/postcss.config.js
@@ -1,13 +1,9 @@
 module.exports = {
-    browsers: "last 2 versions",
     plugins: [
-        require('postcss-import'),
-        require('postcss-preset-env')({
-            stage: 3,
-            features: {
-                'nesting-rules': true
-            }
+        require('postcss-import')(),
+        require('postcss-env-function')({
+            importFrom: './postcss.env.js'
         }),
-        require('autoprefixer')
+        require('precss')()
     ]
 };
